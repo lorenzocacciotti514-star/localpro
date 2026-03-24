a93f767648923d71ba4b1a7831f00c944231f550
Lorenzo <lorenzocacciotti514@gmail.com>
2026-03-24 10:12:50 +0100

    Primo commit

---
+++ localproBE/src/main/java/com/generation/localpro/LocalproApplication.java
@@ -0,0 +1,13 @@
+package com.generation.localpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalproApplication.class, args);
+	}
+
+}
---
+++ localproBE/src/test/java/com/generation/localpro/LocalproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.localpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
